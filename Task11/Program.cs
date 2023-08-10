9cae5e9ff3342890c2bda2853811afacc1d9c7a2
nikitakudko <nikitakudko@gmail.com>
2023-08-10 21:13:36 +0300

    добавил решение task11

---
+++ Task11/Program.cs
@@ -0,0 +1,13 @@
+﻿int number = new Random().Next(100, 1000 );
+Console.WriteLine($"случайное числов в диапазоне 100-1000 -> {number}");
+
+int digit = Digit(number);
+Console.WriteLine($"новое число-> {digit}");
+
+int Digit(int num)
+{
+    int firstdigit = num / 100;
+    int thirddigit = num % 10;
+    int result = firstdigit * 10 + thirddigit;
+    return result;
+}
