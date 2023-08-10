b69c98d892bd47be541c3c8c1e191691ff5f566d
nikitakudko <nikitakudko@gmail.com>
2023-08-10 20:13:51 +0300

    Добавил решение task09

---
+++ Task09/Program.cs
@@ -0,0 +1,12 @@
+﻿int number = new Random().Next(10, 100 );
+Console.WriteLine($"случайное числов в диапазоне 10-99 -> {number}");
+
+int firstdigit = number / 10;
+int seconddigit = number % 10;
+
+int maxdigit = firstdigit;
+if(seconddigit>maxdigit) seconddigit = maxdigit;
+
+Console.WriteLine($"Наибольшая цифра случайного числа из диапазона - {maxdigit}");
+
+    
