9053745cee69704e8c98216ea4d2cdbe95f5493b
nikitakudko <nikitakudko@gmail.com>
2023-08-10 20:41:47 +0300

    Добавил task09

--- Task09/Program.cs
+++ Task09/Program.cs
@@ -1,11 +1,13 @@
 ﻿int number = new Random().Next(10, 100 );
 Console.WriteLine($"случайное числов в диапазоне 10-99 -> {number}");
 
-int firstdigit = number / 10;
-int seconddigit = number % 10;
+int maxDigit = MaxDigit(number);
+Console.WriteLine($"Наибольшая цифра числа-> {maxDigit}");
 
-int result = firstdigit > seconddigit ? firstdigit : seconddigit;
-
-Console.WriteLine($"Наибольшая цифра случайного числа - {result}");
-
-    
+int MaxDigit(int num)
+{
+    int firstdigit = number / 10;
+    int seconddigit = number % 10;
+    int result = firstdigit > seconddigit ? firstdigit : seconddigit;
+    return result;
+}
