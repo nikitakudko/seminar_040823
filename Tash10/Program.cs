dceff509fbf594bed54832d8e695fdb81c7dde3c
nikitakudko <nikitakudko@gmail.com>
2023-08-11 18:55:12 +0300

    Добавил решение task10 and task12

---
+++ Tash10/Program.cs
@@ -0,0 +1,13 @@
+﻿Console.WriteLine("Введите число");
+int number = Convert.ToInt32(Console.ReadLine());
+
+int digit = Digit(number);
+Console.WriteLine($"вторая цифра числа-> {digit}");
+
+int Digit(int num)
+{
+    int firstdigit = number / 10;
+    int seconddigit = firstdigit % 10;
+    int result = seconddigit;
+    return result;
+}
---
+++ Task12/Program.cs
@@ -0,0 +1,14 @@
+﻿Console.WriteLine("Введите число");
+int firnum = Convert.ToInt32(Console.ReadLine());
+
+Console.WriteLine("Введите число");
+int secnum = Convert.ToInt32(Console.ReadLine());
+if(firnum % secnum == 0)
+    {
+        Console.WriteLine("Кратно");
+    }
+    else
+    {
+        int result = firnum % secnum;
+        Console.WriteLine($"{result}");
+    }
