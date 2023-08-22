acc4578789022649dc690f5dae9bb028067c065b
nikitakudko <nikitakudko@gmail.com>
2023-08-22 21:04:51 +0300

    add task 17,18

---
+++ task17/Program.cs
@@ -0,0 +1,28 @@
+﻿/*Напишите программу, которая принимает на вход
+координаты точки (X и Y), причём X ≠ 0 и Y ≠ 0 и выдаёт
+номер четверти плоскости, в которой находится эта
+точка.*/
+int Quarter(int x, int y)
+{
+    if(x > 0 && y > 0) return 1;
+    if(x < 0 && y > 0) return 2;
+    if(x < 0 && y < 0) return 3;
+    if(x > 0 && y < 0) return 4;
+    return 0;
+}
+
+
+
+Console.WriteLine("Введите координаты точки");
+Console.WriteLine("x: ");
+int xCoordinate = Convert.ToInt32(Console.ReadLine());
+Console.WriteLine("y: ");
+int yCoordinate = Convert.ToInt32(Console.ReadLine());
+
+int quarter = Quarter(xCoordinate, yCoordinate);
+
+string result = quarter > 0 ?   
+                $"Указанный координаты соответствуют четверти -> {quarter}"
+                : "Введены некоретные данные";
+
+Console.WriteLine(result);
---
+++ task18/Program.cs
@@ -0,0 +1,8 @@
+﻿string CoordinateRange(int num);
+{
+    if(num == 1) return "x > 0, y > 0 ";
+    if(num == 2) return "x < 0, y > 0 ";
+    if(num == 3) return "x < 0, y < 0 ";
+    if(num == 4) return "x > 0, y < 0 ";
+    return null;
+}
