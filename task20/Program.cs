ac677ffa06193e4157fad57d34a69880ba8e72d1
nikitakudko <nikitakudko@gmail.com>
2023-08-28 20:30:47 +0300

    add task 20  and  task 22

--- task20/Program.cs
+++ task20/Program.cs
@@ -1,2 +1,18 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿double Gipotenuza (int xA, int yA, int xB, int yB)
+{
+    return Math.Sqrt(Math.Pow(xB - xA, 2) + Math.Pow(yB - yA, 2));
+}
+Console.WriteLine("Введите координаты первой точки");
+Console.WriteLine("x: ");
+int xCoordinateA = Convert.ToInt32(Console.ReadLine());
+Console.WriteLine("y: ");
+int yCoordinateA = Convert.ToInt32(Console.ReadLine());
+
+Console.WriteLine("Введите координаты второй точки");
+Console.WriteLine("x: ");
+int xCoordinateB = Convert.ToInt32(Console.ReadLine());
+Console.WriteLine("y: ");
+int yCoordinateB = Convert.ToInt32(Console.ReadLine());
+
+double result = Gipotenuza(xCoordinateA, yCoordinateA, xCoordinateB, yCoordinateB);
+Console.WriteLine($"Длина отрезка: {Math.Round(result, 2, MidpointRounding.ToZero)}");
