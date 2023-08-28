1e4281697569038b8a696c7f0d5022486fbc1603
nikitakudko <nikitakudko@gmail.com>
2023-08-28 20:31:28 +0300

    add  task 22

---
+++ task22/Program.cs
@@ -0,0 +1,16 @@
+﻿
+void TableSquare(int n)
+{
+    int i = 1;
+    while( i<= n)
+    {
+        Console.WriteLine($"{i, 3} -> {i * i, 5}");
+        i++;
+    }
+}
+Console.WriteLine("Введите число");
+int number = Convert.ToInt32(Console.ReadLine());
+
+TableSquare(number);
+
+
